ad7086b62966cddc5dc41996f34e9bbb057d0ad0
rizkymalm <rizkymalm@gmail.com>
2023-12-04 21:22:22 +0700

    wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """ Add  result """
+    return x+y
+
+def subtract(x, y):
+    """ Subtract result """
+    return x - y
---
+++ app/app/test.py
@@ -0,0 +1,19 @@
+"""
+Sample test
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    def test_add_numbers(self):
+        """Test that two numbers are added together"""
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """ test subtracting numbers"""
+        res = calc.subtract(10 , 5)
+
+        self.assertEqual(res, 5)
