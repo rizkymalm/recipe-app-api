9d9757d4e07134e9bd907460aed75482b84c9725
rizkymalm <rizkymalm@gmail.com>
2023-12-04 21:29:48 +0700

    2

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,9 +1,9 @@
 """
 Calculator functions
 """
-def add(x, y):
+def add(x,y):
     """ Add  result """
     return x+y
-def subtract(x, y):
+def subtract(x,y):
     """ Subtract result """
     return x - y
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -11,14 +11,14 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """ Django command to wait for database """
-    def handle(self, *args, **options):
+    def handle(self,*args,**options):
         self.stdout.write('Waiting for database')
         db_up = False
         while db_up is False:
             try:
                 self.check(databases=['default'])
                 db_up = True
-            except (Psycopg2OpError, OperationalError):
+            except (Psycopg2OpError,OperationalError):
                 self.stdout.write('Database unavailable...')
                 time.sleep(1)
 
